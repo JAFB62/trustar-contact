3242af39eb5c3df69eda04ed332efc8af655307d
Joel Barbosa <jafbarbosa2008@gmail.com>
2026-09-04 16:22:20 -0300

    Commit 040926

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/apps/trustar/trustar-contact',
   plugins: [
     vue(),
     vueDevTools(),
